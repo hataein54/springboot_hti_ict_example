5c0e542f50d5e9eed165d7d37ae2ea54e7999964
WIN-TE9T2I6AN4G\ICT02_08 <hahataehaha@gmail.com>
2024-09-23 17:42:59 +0900

    commit

---
+++ springboot_hti_ict_example/src/main/java/edu/ict/ex/controller/EmpController.java
@@ -0,0 +1,86 @@
+package edu.ict.ex.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import edu.ict.ex.page.Criteria;
+import edu.ict.ex.page.PageVO;
+import edu.ict.ex.service.DeptSevice;
+import edu.ict.ex.service.EmpService;
+import edu.ict.ex.vo.EmpVO;
+
+
+@Controller
+@RequestMapping("/emp")
+public class EmpController {
+
+   @Autowired
+   private EmpService empService;
+   
+   @Autowired
+   private DeptSevice  DeptSevice;
+   
+   
+   
+   @GetMapping("/list")
+   public String list(Model model) {
+      
+      System.out.println("list()..");
+      
+      model.addAttribute("empList",empService.getList());
+      
+      return "emp/list";
+   }
+   
+   @GetMapping("/list3")
+   public String list3(Model model) {
+      
+      System.out.println("list3()..");
+      
+      model.addAttribute("empList",empService.getEmpDeptList());
+      
+      return "emp/list3";
+   }
+   
+   
+   @GetMapping("/insert_view")
+   public String insert_view(Model model) {
+      
+      System.out.println("insert_view()..");
+      
+      model.addAttribute("empList", empService.getList());
+      model.addAttribute("deptList",DeptSevice.getList());
+         
+      
+      return "emp/insert";
+   }
+   
+   @PostMapping("/insert")
+   public String insert(EmpVO empVO) {
+      
+      System.out.println("insert()..");
+      System.out.println(empVO);
+      
+      empService.insert(empVO);
+      
+      return "redirect:/emp/list";
+   }
+   
+   @GetMapping("/list2")
+   public String list2(Criteria criteria, Model model) {
+      System.out.println();
+         
+      model.addAttribute("empList", empService.getListWithPaging(criteria));
+      
+      int total = empService.getTotal();
+      model.addAttribute("pageMaker", new PageVO(criteria,total));
+      
+      return "emp/list2";
+   }
+   
+
+}
---
+++ springboot_hti_ict_example/src/main/resources/static/js/board.js
@@ -0,0 +1,4 @@
+
+
+console.log("board 모듈..")
+
